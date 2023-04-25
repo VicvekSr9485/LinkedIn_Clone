5039673f74b943207e6c7738e37f4c586998348b
VicvekSr <Olamideoso65@gmail.com>
2023-04-25 02:12:54 +0000

    add some basic functions

---
+++ linkedin-clone/src/components/LoginComponent.jsx
@@ -0,0 +1,7 @@
+import React
+
+export default function LoginComponent() {
+    return (
+        <div><h1>LoginComponent</h1></div>
+    )
+}
---
+++ linkedin-clone/src/pages/Login.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import LoginComponent from "../components/LoginComponent";
+
+export default function Login() {
+    return <LoginComponent />;
+}
